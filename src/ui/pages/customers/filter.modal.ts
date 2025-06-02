c514371f4774dd4c9f7d76805a4b1a0412c811d3
Viktoryia Kuzmitskaya <kuzmitskaya.viktoryia@gmail.com>
2025-06-03 00:20:57 +0700

    Added test for customer deleteion

--- src/ui/pages/customers/filter.modal.ts
+++ src/ui/pages/customers/filter.modal.ts
@@ -1,13 +1,8 @@
-import { expect } from "@playwright/test";
 import { Modal } from "./modal.page";
 
 export class FilterModal extends Modal {
-  readonly uniqueElement = this.page.locator(`div[role="dialog"]`);
-
-  readonly title = this.uniqueElement.locator(".modal-title");
   readonly applyButton = this.uniqueElement.getByRole("button", { name: "Apply" });
   readonly clearFiltersButton = this.uniqueElement.getByRole("button", { name: "Clear Filters" });
-  readonly closeButton = this.uniqueElement.locator('button[aria-label="Close"]');
 
   readonly checkbox = (name: string) => this.uniqueElement.locator(`input[value="${name}"]`);
 
@@ -23,10 +18,5 @@ export class FilterModal extends Modal {
 
   async clickClearFilters() {
     await this.clearFiltersButton.click();
-  }
-
-  async close() {
-    await this.closeButton.click();
-    await expect(this.uniqueElement).not.toBeVisible();
   }
 }
