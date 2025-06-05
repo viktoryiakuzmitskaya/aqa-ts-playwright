3861216716f8fa2d1c38707df53bf66327908324
Viktoryia Kuzmitskaya <kuzmitskaya.viktoryia@gmail.com>
2025-06-06 00:02:31 +0700

    Updated test with signIn controller

--- src/api/tests/auth/login.spec.ts
+++ src/api/tests/auth/login.spec.ts
@@ -1,20 +1,27 @@
 import { test, expect } from "fixtures/controllers.fixture";
 import { USER_LOGIN, USER_PASSWORD } from "config/environment";
-import { loginSchema } from "data/schemas/auth/login.schema";
 import { STATUS_CODES } from "data/statusCodes";
-import { validateSchema } from "utils/validations/schemaValidation";
 import { validateResponse } from "utils/validations/responseValidation";
 
 
 test.describe("[API] [User] [Login]", () => {
   test("Should allow user login with valid credentials", async ({ signInController }) => {
 
-    const response = await signInController.login({
+    const loginResponse = await signInController.login({
       username: USER_LOGIN,
       password: USER_PASSWORD,
-    });
-
-    validateResponse(response, STATUS_CODES.OK, true, null);   
-    validateSchema(loginSchema, response.body);
+    });    
+    validateResponse(loginResponse, STATUS_CODES.OK, true, null);
+    const token = (loginResponse.headers as Record<string, string>)["authorization"];
+    expect.soft(token).toBeTruthy();
+    const expectedUser = {
+      _id: "680756fcd006ba3d475fc5a3",
+      username: "kuz",
+      firstName: "Viktoryia",
+      lastName: "Kuzmitskaya",
+      roles: ["USER"],
+      createdOn: "2025/04/22 08:44:44",
+    };
+    expect.soft(loginResponse.body.User).toMatchObject(expectedUser);
   });
 });
